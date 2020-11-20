96f45fcfb4d2782d2127f2126ef2a7812ab5f91f
szabinakov <szabinkov@gmail.com>
2020-11-20 16:32:52 +0000

    updated test files after changes for styling

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -1,26 +1,21 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Contact from "../components/Contact";
 
-import React from 'react';
-import { render } from '@testing-library/react';
-import Contact from '../components/Contact';
+describe("Contact", () => {
+  it("matches the Snapshot", () => {
+    const { asFragment } = render(<Contact />);
 
-describe('Contact', () => {
-  it('matches the Snapshot', () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-    const {asFragment} = render (<Contact/>)
+  it("renders all the elements", () => {
+    const { getByTestId } = render(<Contact />);
 
-    expect(asFragment()).toMatchSnapshot()
-  })
-
-  it('renders all the elements', () => {
-      const {getByTestId} = render(<Contact/>)
-
-      expect(getByTestId('name')).toHaveClass('formInputName')
-      expect(getByTestId('email')).toHaveClass('formInputEmail')
-      expect(getByTestId('subject')).toHaveClass('formInputSubject')
-      expect(getByTestId('message')).toHaveClass('formInputMessage')
-      expect(getByTestId('submitButton')).toHaveClass('submitButtonForm')
-      expect(getByTestId('resetButton')).toHaveClass('resetButtonForm')
-  })
-
-})
-
+    expect(getByTestId("name")).toHaveClass("formInputName");
+    expect(getByTestId("email")).toHaveClass("formInputEmail");
+    expect(getByTestId("subject")).toHaveClass("formInputSubject");
+    expect(getByTestId("message")).toHaveClass("textarea");
+    expect(getByTestId("submitButton")).toHaveClass("submitButtonForm");
+  });
+});
--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import {render} from '@testing-library/react'
-import Home from '../components/Home'
+import React from "react";
+import { render } from "@testing-library/react";
+import Home from "../components/Home";
 
-describe('Home', () => {
-    it('matches the Snapshot', () => {
-        const {asFragment} = render(<Home/>)
+describe("Home", () => {
+  it("matches the Snapshot", () => {
+    const { asFragment } = render(<Home />);
 
-        expect(asFragment()).toMatchSnapshot()
-    })
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-    it('renders all elements', () => {
-        const {getByTestId} = render(<Home/>)
+  it("renders all elements", () => {
+    const { getByTestId } = render(<Home />);
 
-        expect(getByTestId('introContainer')).toHaveClass('introContainer')
-        expect(getByTestId('helloDiv')).toHaveClass('helloDiv')
-        expect(getByTestId('myNameDiv')).toHaveClass('myNameDiv')
-        expect(getByTestId('engineerContainer')).toHaveClass('engineerContainer')
-        expect(getByTestId('longAbout')).toHaveClass('longAbout')
-        expect(getByTestId('logoContainer')).toHaveClass('logoContainer')
-    })
-})
+    expect(getByTestId("introContainer")).toHaveClass("introContainer");
+    expect(getByTestId("helloDiv")).toHaveClass("helloDiv");
+    expect(getByTestId("myNameDiv")).toHaveClass("myNameDiv");
+    expect(getByTestId("topCircle")).toHaveClass("topCircle");
+    expect(getByTestId("middleCircle")).toHaveClass("middleCircle");
+    expect(getByTestId("bottomCircle")).toHaveClass("bottomCircle");
+  });
+});
